c5bba544ed6515cf26ce04e1c7d84f9a1c558a69
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-07-16 14:13:40 +0530

    Full Stack project

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-import Homepage from 'scenes/homepage';
-import Loginpage from 'scenes/loginpage';
-import Profilepage from 'scenes/profile page';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginpage";
+import ProfilePage from "scenes/profile page";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Loginpage />} />
-          <Route path="/home" element={<Homepage />} />
-          
-          <Route path="/profile/:userId" element={<Profilepage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
